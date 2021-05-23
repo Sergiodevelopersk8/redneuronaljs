b273be01947b80de2d2890a5e572207e1298b1c9
Sergiodevelopersk8 <sergiodeveloperskate3097@hotmail.com>
2021-05-22 21:02:59 -0500

    color de back

---
+++ js/actualizacion.js
@@ -0,0 +1,7 @@
+function update(color){
+    // console.log(color);
+     var div = document.getElementById("sitio");
+     
+    // div.style.background= color.toHEXString();
+    div.style.background= color;
+     }
